ef5bda1867fdc8d3eee5c8c67c015a1a3a5408ca
Berat MEN <beratmen9@gmail.com>
2025-11-12 00:21:41 +0300

    Version_v1.3
    Co-Authored-By: Berat MEN <beratmen9@gmail.com>

--- next.config.js
+++ next.config.js
@@ -15,9 +15,20 @@ const nextConfig = {
         pathname: '/**',
       }
     ],
-    unoptimized: true
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    minimumCacheTTL: 60,
   },
-  output: 'standalone'
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    optimizeCss: true,
+    scrollRestoration: true
+  }
   // Next.js 14 doesn't support server config in next.config.js
 }
 
